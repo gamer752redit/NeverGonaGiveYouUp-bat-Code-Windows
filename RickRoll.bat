1fe39abc4b1fba4efe39173be739f9e940e19fd9
Gamer752GitHub <165944146+gamer752redit@users.noreply.github.com>
2025-04-10 16:28:29 +0200

    Add files via upload

---
+++ RickRoll.bat
@@ -0,0 +1,22 @@
+@echo off
+title Never Gonna Give You Up
+color 0D
+echo            OOOOOOOOOOO
+echo         OOOOOOOOOOOOOOOOOOO
+echo      OOOOOO  OOOOOOOOO  OOOOOO
+echo    OOOOOO      OOOOO      OOOOOO
+echo  OOOOOOOO  #   OOOOO  #   OOOOOOOO
+echo OOOOOOOOOO    OOOOOOO    OOOOOOOOOO
+echo OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+echo OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+echo OOOO  OOOOOOOOOOOOOOOOOOOOO  OOOO
+echo  OOOO  OOOOOOOOOOOOOOOOOOO  OOOO
+echo    OOOOOO  OOOOOOOOOOOOO  OOOOOO
+echo      OOOOOO  OOOOOOOOO  OOOOOO
+echo         OOOOOOOOOOOOOOOOOOO
+echo             OOOOOOOOOOO
+echo.
+timeout 2 >nul
+start "" "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+echo Never gonna give you up...
+pause
